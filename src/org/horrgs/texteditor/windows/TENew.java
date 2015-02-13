c1f187cd4a45cbf4641eda66ea363765ee61d863
Horrgs <horrgs34@gmail.com>
2015-02-13 15:38:37 -0500

    Window for when "new doc" is selected.

---
+++ src/org/horrgs/texteditor/windows/TENew.java
@@ -0,0 +1,43 @@
+package org.horrgs.texteditor.windows;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Horrgs on 2/13/2015.
+ */
+public class TENew extends JFrame {
+    public JButton currentWindow;
+    public JButton newWindow;
+    /**
+     * This window will come up when you click the button "new"
+     * it'll give you the option to do it in a new window or the
+     * current editor.
+     * @param textEditorArea this is the editor of the area.
+     */
+    public TENew(JTextArea textEditorArea) {
+        setSize(200, 100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        setResizable(false);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        currentWindow = new JButton("Current Editor");
+        currentWindow.setToolTipText("This will use the current editor and remove all text from it.");
+        //TODO: currentWindow addActionListener
+        add(currentWindow, gbc);
+        gbc.gridx = 1;
+        gbc.insets = new Insets(10, 0, 10 ,10);
+        newWindow = new JButton("New Editor");
+        newWindow.setToolTipText("This will open a new editor and keep the current one.");
+        //TODO: newWindow addActionListener
+        add(newWindow, gbc);
+        setVisible(true);
+    }
+    
+    public boolean newWindow() {
+        return false;
+    }
+}
