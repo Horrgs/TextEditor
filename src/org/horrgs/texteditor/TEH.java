76dd1eb3722fa42699319fe514fcf4ab208a8b41
Horrgs <horrgs34@gmail.com>
2015-02-13 23:59:59 -0500

    TO TESTART

--- src/org/horrgs/texteditor/TEH.java
+++ src/org/horrgs/texteditor/TEH.java
@@ -1,12 +1,14 @@
 package org.horrgs.texteditor;
 
 import org.horrgs.texteditor.windows.TEEditor;
+import org.horrgs.texteditor.windows.TEStart;
 
 /**
  * Created by Horrgs on 2/13/2015.
  */
 public class TEH {
     public static void main(String[] args) {
-        new TEEditor();
+        new TEStart();
+        //new TEEditor(null);
     }
 }
