b2c1b3d4bc4ea3553938f4b700b0eab4d191a7fe
Horrgs <horrgs34@gmail.com>
2015-02-15 00:13:23 -0500

    A method I stole from stackoverflow. TODO's also(;

--- src/org/horrgs/texteditor/TEH.java
+++ src/org/horrgs/texteditor/TEH.java
@@ -1,14 +1,47 @@
 package org.horrgs.texteditor;
 
-import org.horrgs.texteditor.windows.TEEditor;
 import org.horrgs.texteditor.windows.TEStart;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Horrgs on 2/13/2015.
  */
 public class TEH {
+    
+    /* TODO: Switch C: to File.listRoots()[0] and / to File.separator.
+    This will provide flexibility amongst cross platforms.
+     */
     public static void main(String[] args) {
         new TEStart();
         //new TEEditor(null);
+    }
+
+    /**
+     @author martinus
+     */
+
+    public static int countLines(String filename) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(filename));
+        try {
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        } finally {
+            is.close();
+        }
     }
 }
