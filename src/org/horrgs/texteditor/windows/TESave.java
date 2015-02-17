648b03f07e0fd388210942d2d3a60aa35e15ec1a
Horrgs <horrgs34@gmail.com>
2015-02-16 21:07:38 -0500

    Save to an existing file and some code optimized.

--- src/org/horrgs/texteditor/windows/TESave.java
+++ src/org/horrgs/texteditor/windows/TESave.java
@@ -10,7 +10,8 @@ import java.io.*;
 /**
  * Created by Horrgs on 2/13/2015.
  */
-public class TESave extends JFrame {
+public class TESave {
+    private JFrame jFrame;
     JTextField hintFileLoc;
     JTextArea fileLocation;
     JTextField hintFileName;
@@ -25,6 +26,7 @@ public class TESave extends JFrame {
     public String aFp;
     private JFrame jfR;
     private File saveFile;
+    //TODO: If the file already exists, bring up prompt with the option to override it.
     /**
      * @param textEditor Text being saved.
      * @param teStart Used when continueWithoutSaving is clicked. Will open the TEStart GUI.
@@ -38,20 +40,21 @@ public class TESave extends JFrame {
         this.aFp = aFp;
         this.teStart = teStart;
         this.textEditor = textEditor;
-        setResizable(false);
-        setSize(600, 400);
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("Saving File...");
-        setLayout(new GridBagLayout());
+        jFrame = new JFrame("");
+        jFrame.setResizable(false);
+        jFrame.setSize(600, 400);
+        jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        jFrame.setTitle("Saving File...");
+        jFrame.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.insets = new Insets(5, 5, 5, 5);
         hintFileLoc = new JTextField("File Location: ");
-        hintFileLoc.setBackground(getBackground());
+        hintFileLoc.setBackground(jFrame.getBackground());
         hintFileLoc.setEditable(false);
-        hintFileLoc.setBorder(BorderFactory.createLineBorder(getBackground()));
-        add(hintFileLoc, gbc);
+        hintFileLoc.setBorder(BorderFactory.createLineBorder(jFrame.getBackground()));
+        jFrame.add(hintFileLoc, gbc);
         gbc.gridx = 1;
         gbc.insets = new Insets(5, 0, 5, 5);
         if(aFp != null) {
@@ -65,21 +68,21 @@ public class TESave extends JFrame {
         fileLocation.setBorder(bf);
         fileLocation.setPreferredSize(new Dimension(267, 18));
         fileLocation.setMinimumSize(new Dimension(267, 18));
-        add(fileLocation, gbc);
+        jFrame.add(fileLocation, gbc);
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.insets = new Insets(0, 5, 5, 5);
         hintFileName = new JTextField("File Name: ");
-        hintFileName.setBackground(getBackground());
+        hintFileName.setBackground(jFrame.getBackground());
         hintFileName.setEditable(false);
-        hintFileName.setBorder(BorderFactory.createLineBorder(getBackground()));
-        add(hintFileName, gbc);
+        hintFileName.setBorder(BorderFactory.createLineBorder(jFrame.getBackground()));
+        jFrame.add(hintFileName, gbc);
         gbc.gridx = 1;
         gbc.insets = new Insets(0, 0, 5, 5);
         fileName.setBorder(bf);
         fileName.setPreferredSize(new Dimension(75, 18));
         fileName.setPreferredSize(new Dimension(75, 18));
-        add(fileName, gbc);
+        jFrame.add(fileName, gbc);
         if(cWs) {
             continueWithoutSaving = new JButton("Continue Without Saving");
             gbc.weightx = 1;
@@ -87,20 +90,20 @@ public class TESave extends JFrame {
             gbc.gridx = 0;
             gbc.gridy = 4;
             continueWithoutSaving.addActionListener(new TESaveListener());
-            add(continueWithoutSaving, gbc);
+            jFrame.add(continueWithoutSaving, gbc);
         }
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.insets = new Insets(0, 5, 5, 5);
         save = new JButton("Save");
         save.addActionListener(new TESaveListener());
-        add(save, gbc);
+        jFrame.add(save, gbc);
         gbc.gridx = 1;
         gbc.insets = new Insets(0, 0, 5, 5);
         cancel = new JButton("Cancel");
         cancel.addActionListener(new TESaveListener());
-        add(cancel, gbc);
-        setVisible(true);
+        jFrame.add(cancel, gbc);
+        jFrame.setVisible(true);
     }
     
     private class TESaveListener implements ActionListener {
@@ -108,15 +111,15 @@ public class TESave extends JFrame {
         @Override
         public void actionPerformed(ActionEvent ev) {
             if(ev.getSource() == cancel) {
-                setVisible(false);
+                jFrame.setVisible(false);
             } else if(ev.getSource() == save) {
-                String a = String.valueOf(fileLocation.getText().charAt(fileLocation.getText().length() - 1));
-                if(!a.equals("/") && !a.equals("\\")) {
+
+                if(!fileLocation.getText().endsWith("/") && !fileLocation.getText().endsWith("\\")) {
                     fileLocation.setText(fileLocation.getText() + "\\");
                 }
                 File f;
                 if(saveFile == null) {
-                    f = new File(fileLocation.getText() + fileName.getText() + ".txt");
+                    f = new File(fileLocation.getText(), fileName.getText() + ".txt");
                 } else {
                     f = saveFile;
                 }
@@ -139,14 +142,15 @@ public class TESave extends JFrame {
                             ex.printStackTrace();
                         }
                     } else {
-                        new Prompt("There is already a file with the name " + f.getName() + " in " + f.getParent());
+                        Prompt p = new Prompt("There is already a file with the name " + f.getName() + " in " + f.getParent(), true, f, jFrame);
+                        p.setText(textEditor.getText().toCharArray());
                     }
                 }
             } else if(ev.getSource() == continueWithoutSaving) {
                 if(teStart) {
                     jfR.setVisible(false);
                     new TEStart();
-                    setVisible(false);
+                    jFrame.setVisible(false);
                 }
             }
         }
