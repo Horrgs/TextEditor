c6668ac8c0e692288f63e5b0bd524d69373a3e40
Horrgs <horrgs34@gmail.com>
2015-02-18 23:27:13 -0500

    I have other obsessions :P. UNFINISHED
    save is being switched to JFileChooser, this is a WIP. May take one more
    commit, two max. This isn't stable and no build should be compiled.

--- src/org/horrgs/texteditor/windows/TESave.java
+++ src/org/horrgs/texteditor/windows/TESave.java
@@ -2,6 +2,7 @@ package org.horrgs.texteditor.windows;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,10 +13,10 @@ import java.io.*;
  */
 public class TESave {
     private JFrame jFrame;
-    JTextField hintFileLoc;
-    JTextArea fileLocation;
-    JTextField hintFileName;
-    JTextField fileName;
+    //JTextField hintFileLoc;
+    //JTextArea fileLocation;
+    //JTextField hintFileName;
+    //JTextField fileName;
     JTextArea textEditor;
     Border bf = BorderFactory.createLineBorder(Color.BLACK);
     public JButton save;
@@ -47,42 +48,6 @@ public class TESave {
         jFrame.setTitle("Saving File...");
         jFrame.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.insets = new Insets(5, 5, 5, 5);
-        hintFileLoc = new JTextField("File Location: ");
-        hintFileLoc.setBackground(jFrame.getBackground());
-        hintFileLoc.setEditable(false);
-        hintFileLoc.setBorder(BorderFactory.createLineBorder(jFrame.getBackground()));
-        jFrame.add(hintFileLoc, gbc);
-        gbc.gridx = 1;
-        gbc.insets = new Insets(5, 0, 5, 5);
-        if(aFp != null) {
-            saveFile = new File(aFp);
-            fileLocation = new JTextArea(saveFile.getParent());
-            fileName = new JTextField(saveFile.getName());
-        } else {
-            fileLocation = new JTextArea("");
-            fileName = new JTextField("");
-        }
-        fileLocation.setBorder(bf);
-        fileLocation.setPreferredSize(new Dimension(267, 18));
-        fileLocation.setMinimumSize(new Dimension(267, 18));
-        jFrame.add(fileLocation, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(0, 5, 5, 5);
-        hintFileName = new JTextField("File Name: ");
-        hintFileName.setBackground(jFrame.getBackground());
-        hintFileName.setEditable(false);
-        hintFileName.setBorder(BorderFactory.createLineBorder(jFrame.getBackground()));
-        jFrame.add(hintFileName, gbc);
-        gbc.gridx = 1;
-        gbc.insets = new Insets(0, 0, 5, 5);
-        fileName.setBorder(bf);
-        fileName.setPreferredSize(new Dimension(75, 18));
-        fileName.setPreferredSize(new Dimension(75, 18));
-        jFrame.add(fileName, gbc);
         if(cWs) {
             continueWithoutSaving = new JButton("Continue Without Saving");
             gbc.weightx = 1;
@@ -114,7 +79,34 @@ public class TESave {
                 jFrame.setVisible(false);
             } else if(ev.getSource() == save) {
 
-                if(!fileLocation.getText().endsWith("/") && !fileLocation.getText().endsWith("\\")) {
+                JFileChooser jFileChooser = new JFileChooser("");
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+                jFileChooser.setFileFilter(filter);
+                if(aFp != null) {
+                    jFileChooser.setCurrentDirectory(new File(aFp));
+                } else {
+                    String s = File.separator;
+                    jFileChooser.setCurrentDirectory(new File(File.listRoots()[0] + ":" + s + "Users" + System.getProperty("user.name") + s + "Desktop" + s));
+                }
+                int r = jFileChooser.showSaveDialog(null);
+                if(r == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(jFileChooser.getSelectedFile() + ".txt")));
+                        for(int x = 0; x < textEditor.getText().length(); x++) {
+                            char[] text = textEditor.getText().toCharArray();
+                            if(text[x] != '\n') {
+                                writer.println(text[x]);
+                                writer.flush();
+                            } else {
+                                writer.print("");
+                            }
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                
+               /* if(!fileLocation.getText().endsWith("/") && !fileLocation.getText().endsWith("\\")) {
                     fileLocation.setText(fileLocation.getText() + File.separator);
                 }
                 File f;
@@ -151,7 +143,7 @@ public class TESave {
                     jfR.setVisible(false);
                     new TEStart();
                     jFrame.setVisible(false);
-                }
+                }              */
             }
         }
     }
